993c80c3aec40ed15290ea8027e2a23ca302b58c
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-06-04 21:47:34 +0530

    Time: 0 ms (100%), Space: 46 MB (48.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,102 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-
+        int low=0;
+        int high=nums.length-1;
         int first=-1;
         int last=-1;
-       
-        int[] result={-1,-1};
 
-        for(int i=0; i<nums.length; i++){
-
-            if(nums[i]==target){
-
-                if(result[0]==-1){
-                    result[0]=i;
-                }
-               
-                    result[1]=i;
-                
-                
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if(nums[mid]==target){
+                first=mid;
+                high=mid-1;
 
             }
-           
-
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
 
-        return result; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if(nums.length==0){
-        //     return new int[]{-1,-1};
-        // }
-        // if(nuns[0])
-
-        // int high=nums.length-1;
-        // int low=0;
-        // int x=0;
-
-        // while(low<=high){
-
-        //     int mid=low+(high-low)/2;
-
-        //     if(nums[mid]>=target){
-        //         high=mid-1;
-        //     }
-        //     else{
-        //         low=mid+1;
-        //     }
-            
-
-        // }
-        // int low1=0;
-        // int high1=nums.length-1;
-        // int UpperB=0;
-
-        // while(low1<=high1){
-        //   int  mid=low1+(high1-low1)/2;
-
-
-        //     if(nums[mid]>target){
-        //         UpperB=mid;
-        //         high1=mid-1;
-        //     }
-
-        //     else{
-        //         low1=mid+1;
-        //     }
-        // }
-
-        // if(low==1||UpperB==nums.length){
-        //     return new int[]{-1,-1};
-        // }
+        int low1=0;
+        int high1=nums.length-1;
         
+        
+           while(low1<=high1){
+            int mid= low1+(high1-low1)/2;
+            if(nums[mid]==target){
+                last=mid;
+                low1=mid+1;
 
+            }
+            else if(nums[mid]>target){
+                high1=mid-1;
+            }
+            else{
+                low1=mid+1;
+            }
+        }
 
-
-
-
-
-
-        // return new int[]{low,UpperB-1};
-
+        return  new int[] {first,last};
         
     }
 }
