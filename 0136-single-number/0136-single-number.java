7245e0654339454ed0e131663d75a0f31565a26c
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-06-11 23:28:09 +0530

    Time: 1 ms (99.92%), Space: 46.1 MB (70.97%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,30 +1,60 @@
 class Solution {
     public int singleNumber(int[] nums) {
+
+
+        int result=0;
+
+        for(int num:nums){
+
+            result ^=num;
+        }
+        return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        int sum=0;
+        // int sum=0;
 
-        for(int i=0; i<nums.length-1; i++){
+        // for(int i=0; i<nums.length-1; i++){
 
 
-            for(int j=i+1; j<nums.length; j++){
+        //     for(int j=i+1; j<nums.length; j++){
 
-                if(nums[i]==nums[j]){
-                    nums[i]=0;
-                    nums[j]=0;
+        //         if(nums[i]==nums[j]){
+        //             nums[i]=0;
+        //             nums[j]=0;
                     
-                }
-
-
-
-
-            }
+        //         }
+        //     }
             
-        }
-        for(int i=0; i<nums.length; i++){
-           sum+=nums[i];
-        }
+        // }
+        // for(int i=0; i<nums.length; i++){
+        //    sum+=nums[i];
+        // }
 
-        return sum;
+        // return sum;
         
     }
 }
