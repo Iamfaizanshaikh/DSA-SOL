8afefa04bda4576b97a96bdb66984318dfc34c66
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-06-12 16:24:31 +0530

    Time: 0 ms (100%), Space: 43 MB (91.36%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,117 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        boolean bool= false;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                bool= true;
-                break;
-            }
-        }
-        return bool;
+int low=0;
+int high=nums.length-1;
+boolean bool=false;
+
+while(low<=high){
+    int mid= low+(high-low)/2;
+
+    if(nums[mid]==target){
+        bool=true;
+       break;
+
+    }
+
+    if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+        low++;
+        high--;
+        continue;
+    }
+ 
+
+    if(nums[low]<=nums[mid]){
+
+        if(target>=nums[low] && target<=nums[mid]){
+
+        high=mid-1;
+
+
+    }
+    else{
+        low=mid+1;
+    }
+    }
+
+   
+    else{
+
+
+ if(target>=nums[mid] && target<=nums[high]){
+        low=mid+1;
+
+    }
+    else{
+        high=mid-1;
+    }
+
+    }
+
+
+}
+return bool;
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int start=0;
+        // int end=nums.lemgth-1;
+        // while(start<end){
+        //     int temp=nums[start];
+        //     nums[start]=nums[end];
+        //     nums[end]=temp;
+        //     start++;
+        //     end--;
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // boolean bool= false;
+        // int j=nums.length-1;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]==target || nums[j]==target){
+        //         bool= true;
+        //         break;
+        //     }
+        //     j--;
+        // }
+        // return bool;
         
     }
 }
