2158a1ab806186751387f0e2e83ba0c9995cddff
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-06-24 19:00:14 +0530

    Time: 2 ms (10.78%), Space: 41.9 MB (68.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,64 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        for(int i=0; i<nums.length-1; i++){
+        int zcount=0;
+        int ocount=0;
+        int tcount=0;
 
-            for(int j=i+1; j<nums.length; j++){
+        int z=0;
 
-                if(nums[i]>nums[j]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                    
-
-                }
-
-
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                zcount++;
+            }
+             if(nums[i]==1){
+                ocount++;
+            }
+             if(nums[i]==2){
+                tcount++;
             }
         }
+
+        for(int j=0; j<zcount; j++){
+            nums[z]=0;
+            z++;
+
+        }
+          for(int j=0; j<ocount; j++){
+            nums[z]=1;
+            z++;
+
+        }
+          for(int j=0; j<tcount; j++){
+            nums[z]=2;
+            z++;
+
+        }
+
+
+
+      
+
+
+
+
+
+
+        // for(int i=0; i<nums.length-1; i++){
+
+        //     for(int j=i+1; j<nums.length; j++){
+
+        //         if(nums[i]>nums[j]){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+                    
+
+        //         }
+
+
+        //     }
+        // }
 
         System.out.println(Arrays.toString(nums));
         
