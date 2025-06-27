db38dbf9d431749d3e3595c770dd66dea87d4104
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-06-28 01:35:33 +0530

    Time: 2 ms (10.9%), Space: 42.1 MB (49.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,61 +1,62 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        // int zcount=0;
-        // int ocount=0;
-        // int tcount=0;
-
-        // int z=0;
-
-        // for(int i=0; i<nums.length; i++){
-        //     if(nums[i]==0){
-        //         zcount++;
-        //     }
-        //      if(nums[i]==1){
-        //         ocount++;
-        //     }
-        //      if(nums[i]==2){
-        //         tcount++;
-        //     }
-        // }
-
-        // for(int j=0; j<zcount; j++){
-        //     nums[z]=0;
-        //     z++;
-
-        // }
-        //   for(int j=0; j<ocount; j++){
-        //     nums[z]=1;
-        //     z++;
-
-        // }
-        //   for(int j=0; j<tcount; j++){
-        //     nums[z]=2;
-        //     z++;
-
-        // }
-
         
-        int zcount = 0;
-        int ocount = 0;
-        int tcount = 0;
+    
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) zcount++;
-            else if (nums[i] == 1) ocount++;
-            else if (nums[i] == 2) tcount++;
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+
+            if (nums[mid] == 0) {
+
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+
+                
+                low++;
+                mid++;
+                }
+
+            else if (nums[mid] == 1) {
+                mid++;
+                }
+
+            else  {
+
+                int temp= nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            
+            
+            
+            
+            }
+
+
+
         }
 
-        int z = 0;
-        for (int j = 0; j < zcount; j++) nums[z++] = 0;
-        for (int j = 0; j < ocount; j++) nums[z++] = 1;
-        for (int j = 0; j < tcount; j++) nums[z++] = 2;
-
-
+      
 
       
 
 
+
+
+          // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == 0) zcount++;
+        //     else if (nums[i] == 1) ocount++;
+        //     else if (nums[i] == 2) tcount++;
+        // }
+
+        // int z = 0;
+        // for (int j = 0; j < zcount; j++) nums[z++] = 0;
+        // for (int j = 0; j < ocount; j++) nums[z++] = 1;
+        // for (int j = 0; j < tcount; j++) nums[z++] = 2;
 
 
 
