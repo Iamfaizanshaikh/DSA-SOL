e04b14522d440fc62af1fe59c8daaca0e303242b
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-09-16 11:30:25 +0530

    Time: 1 ms (99.81%), Space: 53.2 MB (50.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,48 +1,42 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
+
+ 
+
        int n= nums.length;
-     
-      
+       int me=0;
+       int vote=0;
+       for(int i=0; i<n; i++){
+        if(vote==0){
+            me=nums[i];
+        }
+        if(me==nums[i]){
+            vote++;
+        }
+        else{
+            vote--;
+        }
+       }
+        
+            // for(int i=0; i<n; i++){
+            //     int count=0;
+            //     for(int j=0; j<n; j++){
 
-
-           
-            for(int i=0; i<n; i++){
-                int count=0;
-
-
-                for(int j=0; j<n; j++){
-
-                      if(nums[i]==nums[j]){
-                        count++;
-                    
-                    
-                }
-
-
-
-
-                }
+            //           if(nums[i]==nums[j]){
+            //             count++;    
+            //     }
+            //     }
 
               
-              if(count>(n/2)){
+            //   if(count>(n/2)){
                 
-                return nums[i];
-              }
-
-
-
-
-            }
+            //     return nums[i];
+            //   }
+            // }
         
        
-        return -1;
-
-
-
-
-
-
-        
+        return me;
+    
     }
 }
