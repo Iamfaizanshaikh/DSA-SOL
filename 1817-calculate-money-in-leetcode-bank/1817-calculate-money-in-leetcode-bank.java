6f3facaeebb877b57f10ff69127ea1c84aa8617f
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-10-25 19:17:28 +0530

    Time: 1 ms (56.85%), Space: 41.1 MB (6.06%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int totalMoney(int n) {
+        int total=0;
+        int start=1;
+        int week=1;
+        for(int i=1; i<=n; i++){
+            total+=start;
+            start++;
+            if(i%7==0){
+                week++;
+                start=week;
+            }
+            
+        }
+        return total;
+    }
+}
