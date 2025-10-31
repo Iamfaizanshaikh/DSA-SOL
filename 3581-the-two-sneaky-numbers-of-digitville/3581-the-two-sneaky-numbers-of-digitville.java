bf18d48f23af9c0059f4c1456fba711f5a902a19
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-10-31 11:33:43 +0530

    Time: 1 ms (100%), Space: 44.7 MB (77.02%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,9 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
+        // HashMap<Integer, Integer> freq = new HashMap<>();
+        // int[] result = new int[2];
+        // int index = 0;
+
+        // for (int num : nums) {
+        //     freq.put(num, freq.getOrDefault(num, 0) + 1);
+            
+        //     // when a number appears the 2nd time → it's sneaky
+        //     if (freq.get(num) == 2) {
+        //         result[index++] = num;
+                
+        //         // once we find 2 sneaky numbers, stop early
+        //         if (index == 2) break;
+        //     }
+        // }
+
+        // return result;
+
+
         int[] arr= new int[2];
         int k=0;
-        
-
         for(int i=0; i<=nums.length-2; i++){
 
             for(int j=i+1; j<=nums.length-1; j++){
