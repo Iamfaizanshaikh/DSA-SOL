3f4b3e66f3a278a3a351591de83e9a5215c82576
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-11-04 23:48:37 +0530

    Time: 28 ms (30.33%), Space: 48.9 MB (5.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,84 +1,84 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
 
-        // int count = 0;
-        // int sum = 0;
+        int count = 0;
+        int sum = 0;
 
-        // Map<Integer, Integer> map = new HashMap<>();
-        // map.put(0, 1);  // important: to handle case when subarray itself starts from index 0
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);  
 
-        // for (int num : nums) {
-        //     sum += num;
+        for (int num : nums) {
+            sum += num;
 
-        //     // Check if (sum - k) happened before
-        //     if (map.containsKey(sum - k)) {
-        //         count += map.get(sum - k);
-        //     }
-
-        //     // Update map: how many times this sum has occurred
-        //     map.put(sum, map.getOrDefault(sum, 0) + 1);
-        // }
-
-        // return count;
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        int count=0;
-
-        for(int i=0; i<nums.length; i++){
-            int sum=0;
-
-               for(int j=i; j<nums.length; j++){
-
-                sum+=nums[j];
-              
-                if(sum==k){    
-                   count++;  
-                   
-                }
-                
+            
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
             }
 
+           
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
+
         return count;
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        // int count=0;
+
+        // for(int i=0; i<nums.length; i++){
+        //     int sum=0;
+
+        //        for(int j=i; j<nums.length; j++){
+
+        //         sum+=nums[j];
+              
+        //         if(sum==k){    
+        //            count++;  
+                   
+        //         }
+                
+        //     }
+
+        // }
+        // return count;
         
     }
 }
