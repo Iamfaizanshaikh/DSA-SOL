ec03a048565cad85b2295333c146b61027c1c569
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2025-11-08 18:24:43 +0530

    Time: 2 ms (99.83%), Space: 52.7 MB (5.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,28 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
     List<Integer> list=new ArrayList<>();
-    if(nums.length==0 || nums==null ) return list;
-    int n=nums.length/3;
-    
-      
-    for(int i=0;i<nums.length;i++){
-        int count=0;
-        if(list.contains(nums[i])) continue;
+    if(nums.length==0 || nums==null ) return list; 
+    int cand1=0, cand2=1;
+    int count1=0, count2=0;
 
-        for(int j=0; j<nums.length; j++){
-            
-            if(nums[i]==nums[j]){
-                count++;
+
+
+          for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+            else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
-
-        }
-        if(count>n){
-            list.add(nums[i]);
-            
         }
 
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+        }
 
-    }
+        int n = nums.length;
+        if (count1 > n / 3) list.add(cand1);
+        if (count2 > n / 3) list.add(cand2);
+
+
+    
     return list;
         
       
