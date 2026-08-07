d31806a8d4ab80e7bc7bf47d9a4ca365c5a35151
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-08-07 17:07:22 +0530

    Time: 2 ms (24.62%), Space: 43.7 MB (63.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n==0){
-            return;
-        }else if(m==0){
-           for(int i=0;i<n;i++){
-            nums1[i]=nums2[i];
-           }
-            return;
-        }
+        // if(n==0){
+        //     return;
+        // }else if(m==0){
+        //    for(int i=0;i<n;i++){
+        //     nums1[i]=nums2[i];
+        //    }
+        //     return;
+        // }
         int i=m-1;
         int j=n-1;
         int k=(m+n)-1;
@@ -40,14 +40,7 @@ class Solution {
 
    System.out.println(Arrays.toString(nums1));
     
-//nums1 =
-// [2,0]
-// m =
-// 1
-// nums2 =
-// [1]
-// n =
-// 1
+
         
     }
 }
