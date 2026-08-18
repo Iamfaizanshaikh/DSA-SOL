eaac0c5cd742cef03ad228123cf56f59182988f8
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-08-18 22:50:27 +0530

    Time: 0 ms (100%), Space: 44.8 MB (82.95%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,117 +1,45 @@
 class Solution {
     public boolean search(int[] nums, int target) {
 
-int low=0;
-int high=nums.length-1;
-boolean bool=false;
+        int left = 0;
+        int right = nums.length - 1;
 
-while(low<=high){
-    int mid= low+(high-low)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-    if(nums[mid]==target){
-        bool=true;
-       break;
+            if (nums[mid] == target) {
+                return true;
 
-    }
+            }
 
-    if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-        low++;
-        high--;
-        continue;
-    }
- 
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+                continue;
+            }
 
-    if(nums[low]<=nums[mid]){
+            if (nums[mid] <= nums[right]) {
 
-        if(target>=nums[low] && target<=nums[mid]){
+                if (target >= nums[mid] && target <= nums[right]) {
 
-        high=mid-1;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
 
+            }
 
-    }
-    else{
-        low=mid+1;
-    }
-    }
+            else {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
 
-   
-    else{
+            }
 
+        }
+        return false;
 
- if(target>=nums[mid] && target<=nums[high]){
-        low=mid+1;
-
-    }
-    else{
-        high=mid-1;
-    }
-
-    }
-
-
-}
-return bool;
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int start=0;
-        // int end=nums.lemgth-1;
-        // while(start<end){
-        //     int temp=nums[start];
-        //     nums[start]=nums[end];
-        //     nums[end]=temp;
-        //     start++;
-        //     end--;
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // boolean bool= false;
-        // int j=nums.length-1;
-        // for(int i=0; i<nums.length; i++){
-        //     if(nums[i]==target || nums[j]==target){
-        //         bool= true;
-        //         break;
-        //     }
-        //     j--;
-        // }
-        // return bool;
-        
     }
 }
