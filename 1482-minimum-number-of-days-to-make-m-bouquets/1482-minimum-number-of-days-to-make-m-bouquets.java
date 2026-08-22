101287c6d3172f8770ab3b1089e82a29001f8b1a
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-08-22 21:16:07 +0530

    Time: 15 ms (97.49%), Space: 82.4 MB (83.61%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,17 +1,20 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         if((long)m*k>bloomDay.length) return -1;
-        int max=0;
+
+        int maxDay=0;
         for(int i=0; i<bloomDay.length; i++){
-            max=Math.max(max, bloomDay[i]);
+            maxDay=Math.max(maxDay, bloomDay[i]);
         }
+
         int left=0;
-        int right=max;
+        int right=maxDay;
 
         while(left<right){
             int mid=left+(right-left)/2;
             int consecutive=0;
             int bouquets=0;
+
             for(int i=0; i<bloomDay.length; i++){
                 if(bloomDay[i]<=mid){
                     consecutive++;
@@ -23,20 +26,35 @@ class Solution {
                 else{
                     consecutive=0;
                 }
-
             }
-
             if(bouquets>=m){
                 right=mid;
             }
             else{
                 left=mid+1;
             }
+
+
+
         }
+
         return left;
-
-
-
-        
+          
     }
 }
+
+
+  
+      
+       
+
+      
+           
+            
+                
+              
+             
+
+          
+           
+        
