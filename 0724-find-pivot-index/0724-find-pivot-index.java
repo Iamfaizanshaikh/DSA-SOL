f4a03ac1c19ed7a53168d5037ba858cf81b5a252
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-08-26 00:47:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,32 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int[] prefix= new int[nums.length];
-        prefix[0]=nums[0];
-        int sum=prefix[0];
-        for(int i=1; i<nums.length;  i++){
-            prefix[i]=sum+nums[i];
-            sum=prefix[i];
+
+
+        int total=0;
+        for(int num: nums){
+            total+=nums[num];
         }
-        int len=nums.length-1;
-        if(prefix[len]-prefix[0]==0) return 0;
+        int leftsum=0;
+
+        for(int i=0; i<nums.length; i++){
+            int rightsum=total-leftsum-nums[i];
+            if(leftsum==rightsum) return i;
+            leftsum+=nums[i];
+        }
+
+
+
+        // int[] prefix= new int[nums.length];
+        // prefix[0]=nums[0];
+        // for(int i=1; i<nums.length;  i++){
+        //     prefix[i]=prefix[i-1]+nums[i];
+        // }
+        // int len=nums.length-1;
+        // if(prefix[len]-prefix[0]==0) return 0;
         
-        for(int i=1; i<nums.length; i++){
-            if(prefix[i-1]==prefix[len]-prefix[i]) return i;
-        } 
+        // for(int i=1; i<nums.length; i++){
+        //     if(prefix[i-1]==prefix[len]-prefix[i]) return i;
+        // } 
 
         return -1;
         
