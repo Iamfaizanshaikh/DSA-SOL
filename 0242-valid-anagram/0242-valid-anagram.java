8f933b3c127145d3be3ae7851ac32f2a84dde330
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-06 12:21:05 +0530

    Time: 24 ms (5.2%), Space: 44.5 MB (66.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,38 +3,22 @@ class Solution {
           if (s.length() != t.length()) {
             return false;
         }
-        Map<Character,Integer> mapS= new HashMap<>();
+        Map<Character,Integer> map= new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
-            char ch=s.charAt(i);
-            if(mapS.containsKey(ch)){
-                mapS.put(ch, mapS.get(ch)+1);
-            }
-            else{
-                mapS.put(ch,1);
-            }
-
+            char sc=s.charAt(i);
+            char tc=t.charAt(i);
+            map.put(sc,map.getOrDefault(sc,0)+1);
+            map.put(tc,map.getOrDefault(tc,0)-1);
+           
         }
-
-        for(int i=0; i<t.length(); i++){
-            char ch= t.charAt(i);
-            if(!mapS.containsKey(ch)){
+        for(int value:map.values()){
+            if(value!=0){
                 return false;
             }
-            int count=mapS.get(ch);
-            if(count==1){
-                mapS.remove(ch);
-            }
-            else{
-                mapS.put(ch,count-1);
-            }
-
-       
-            
         }
 
-
-        return mapS.isEmpty();
+        return true;
      
     }
 }
