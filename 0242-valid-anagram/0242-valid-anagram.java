3606d6dbf1e752ecc11c53b3f1febcbc0e59f60f
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-06 12:34:34 +0530

    Time: 6 ms (44.03%), Space: 44.1 MB (94.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,20 +3,29 @@ class Solution {
           if (s.length() != t.length()) {
             return false;
         }
-        Map<Character,Integer> map= new HashMap<>();
-
+        int[] count= new int[26];
         for(int i=0; i<s.length(); i++){
-            char sc=s.charAt(i);
-            char tc=t.charAt(i);
-            map.put(sc,map.getOrDefault(sc,0)+1);
-            map.put(tc,map.getOrDefault(tc,0)-1);
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int v:count){
+            if(v!=0) return false;
+        }
+        // Map<Character,Integer> map= new HashMap<>();
+
+        // for(int i=0; i<s.length(); i++){
+        //     char sc=s.charAt(i);
+        //     char tc=t.charAt(i);
+        //     map.put(sc,map.getOrDefault(sc,0)+1);
+        //     map.put(tc,map.getOrDefault(tc,0)-1);
            
-        }
-        for(int value:map.values()){
-            if(value!=0){
-                return false;
-            }
-        }
+        // }
+        // for(int value:map.values()){
+        //     if(value!=0){
+        //         return false;
+        //     }
+        // }
 
         return true;
      
