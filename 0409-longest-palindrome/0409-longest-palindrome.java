bb171d94853db26a376865625cd05e74074e6371
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-07 23:04:05 +0530

    Time: 3 ms (64.58%), Space: 43.1 MB (56.56%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,23 +1,63 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character,Integer> map= new HashMap<>();
 
+        int[] freq=new int[128];
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            freq[ch]++;
+            
         }
         int count=0;
-        int max=0;
         boolean hasOdd=false;
-        for(int freq: map.values()){
-            if(freq % 2 == 0){
-                count+=freq;
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]%2==0){
+                count+=freq[i];
             }
             else{
-                count +=freq-1;
+                count+=freq[i]-1;
                 hasOdd=true;
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Map<Character,Integer> map= new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     char ch=s.charAt(i);
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+        // }
+        // int count=0;
+        // int max=0;
+        // boolean hasOdd=false;
+        // for(int freq: map.values()){
+        //     if(freq % 2 == 0){
+        //         count+=freq;
+        //     }
+        //     else{
+        //         count +=freq-1;
+        //         hasOdd=true;
+        //     }
+        // }
+
         return count+ (hasOdd? 1: 0);
         
     }
