97687c7367498b6d9915716cc0ac270c42d6fad7
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-07 21:13:30 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.57%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,55 +1,94 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)==' '){
-                count++;
+
+        String[] words= s.split(" ");
+        if(words.length!=pattern.length()) return false;
+
+        Map<Character, String> map= new HashMap<>();
+        for(int i=0; i<words.length; i++){
+            char ch=pattern.charAt(i);
+            String word= words[i];
+
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(word)) return false;
+
+            }else{
+                if(map.containsValue(word)) return false;
+                map.put(ch,word);
             }
+
         }
-        if(pattern.length()!=count+1) return false;
 
-        Map<String,Character> map= new HashMap<>();
-        int j=0;
-        StringBuilder st= new StringBuilder();
 
-        for(int i=0; i<s.length(); i++){  
-            if(s.charAt(i)==' '){
-                String word= st.toString();
-                char ch=pattern.charAt(j);
 
-                if(map.containsKey(word)){
-                    if(map.get(word)!=ch) return false;
 
-                } else{
-                    if(map.containsValue(ch)){
-                        return false;
-                    }
-                    map.put(word,ch);
-                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int count=0;
+        // for(int i=0; i<s.length(); i++){
+        //     if(s.charAt(i)==' '){
+        //         count++;
+        //     }
+        // }
+        // if(pattern.length()!=count+1) return false;
+
+        // Map<String,Character> map= new HashMap<>();
+        // int j=0;
+        // StringBuilder st= new StringBuilder();
+
+        // for(int i=0; i<s.length(); i++){  
+        //     if(s.charAt(i)==' '){
+        //         String word= st.toString();
+        //         char ch=pattern.charAt(j);
+
+        //         if(map.containsKey(word)){
+        //             if(map.get(word)!=ch) return false;
+
+        //         } else{
+        //             if(map.containsValue(ch)){
+        //                 return false;
+        //             }
+        //             map.put(word,ch);
+        //         }
                 
-                st.setLength(0);
-                j++;
+        //         st.setLength(0);
+        //         j++;
 
-            }
-            else{
-                st.append(s.charAt(i));
-            }
-        }
-        if (st.length() > 0) {
-            String word = st.toString();
-            char ch = pattern.charAt(j);
+        //     }
+        //     else{
+        //         st.append(s.charAt(i));
+        //     }
+        // }
+        // if (st.length() > 0) {
+        //     String word = st.toString();
+        //     char ch = pattern.charAt(j);
 
-            if (map.containsKey(word)) {
-                if (map.get(word) != ch) {
-                    return false;
-                }
-            } else {
-                if (map.containsValue(ch)) {
-                    return false;
-                }
-                map.put(word, ch);
-            }
-        }
+        //     if (map.containsKey(word)) {
+        //         if (map.get(word) != ch) {
+        //             return false;
+        //         }
+        //     } else {
+        //         if (map.containsValue(ch)) {
+        //             return false;
+        //         }
+        //         map.put(word, ch);
+        //     }
+        // }
 
         return true;
 
