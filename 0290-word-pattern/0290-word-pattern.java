10521b2c6598fd3964f2d673226c7b7990f781e5
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-07 20:10:16 +0530

    Time: 1 ms (83.88%), Space: 43 MB (18.38%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==' '){
+                count++;
+            }
+        }
+        if(pattern.length()!=count+1) return false;
+
+        Map<String,Character> map= new HashMap<>();
+        int j=0;
+        StringBuilder st= new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){  
+            if(s.charAt(i)==' '){
+                String word= st.toString();
+                char ch=pattern.charAt(j);
+
+                if(map.containsKey(word)){
+                    if(map.get(word)!=ch) return false;
+
+                } else{
+                    if(map.containsValue(ch)){
+                        return false;
+                    }
+                    map.put(word,ch);
+                }
+                
+                st.setLength(0);
+                j++;
+
+            }
+            else{
+                st.append(s.charAt(i));
+            }
+        }
+        if (st.length() > 0) {
+            String word = st.toString();
+            char ch = pattern.charAt(j);
+
+            if (map.containsKey(word)) {
+                if (map.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(ch)) {
+                    return false;
+                }
+                map.put(word, ch);
+            }
+        }
+
+        return true;
+
+        
+    }
+}
