9409fe9666ed95b11af304ca1d44cc985ab10606
Mohammad Faizan <100222348+Iamfaizanshaikh@users.noreply.github.com>
2026-09-23 19:50:03 +0530

    Time: 2 ms (63.3%), Space: 43.1 MB (96.05%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,34 +1,37 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
-        if(s.equals(goal)) return true;
 
-        char[] ch= new char[s.length()];
-        char one=goal.charAt(0);
-        int place=0;
+        return(s.length()==goal.length() && (s+s).contains(goal));
 
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)==one){
-                int left=0;
-                int num=i;
+        // if(s.length()!=goal.length()) return false;
+        // if(s.equals(goal)) return true;
 
-                while(left<s.length()){
-                    ch[left]=s.charAt(num);
-                    num=(num+1)%s.length();
-                    left++;
-                }
+        // char[] ch= new char[s.length()];
+        // char one=goal.charAt(0);
+        // int place=0;
 
-                String temp= new String(ch);
+        // for(int i=0; i<s.length(); i++){
+        //     if(s.charAt(i)==one){
+        //         int left=0;
+        //         int num=i;
 
-                if(temp.equals(goal)){
-                    return true;
-                }
+        //         while(left<s.length()){
+        //             ch[left]=s.charAt(num);
+        //             num=(num+1)%s.length();
+        //             left++;
+        //         }
+
+        //         String temp= new String(ch);
+
+        //         if(temp.equals(goal)){
+        //             return true;
+        //         }
               
                 
                
-            }
-        }
-        return false;
+        //     }
+        // }
+        // return false;
         
         
     }
